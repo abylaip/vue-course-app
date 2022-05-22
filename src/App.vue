f625734176b2973dd564d54c5b263022e94e0085
Abylay Aiyp <ablyaip@gmail.com>
2022-05-22 12:48:30 +0600

    feat:components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <post-form @create="createPost" />
+    <post-list :posts="posts" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld,
+    PostForm,
+    PostList,
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
 }
 </style>
